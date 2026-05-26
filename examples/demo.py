9a17e32286f11a66c059b78b997b6f4f6d5563d6
Felix P. Broesamle <felix.broesamle.research@gmail.com>
2026-05-26 14:22:02 +0200

    Minor

--- examples/demo.py
+++ examples/demo.py
@@ -112,6 +112,7 @@ if __name__ == "__main__":
 
     loaded_spread_mc_rand_caps_costs_data = s2mflow.load_multi_commodity_instance(output_path)
 
+
     data = {1: 126, 2: -126}
 
     spread_multi_data = s2mflow.split_supplies_spread(
